53bf82c76c0aaf515a97fa340a396e760b4d9111
WuangTX <quang9b1234@gmail.com>
2025-07-07 18:56:36 +0700

    moi

--- vav_furniture/settings.py
+++ vav_furniture/settings.py
@@ -129,7 +129,7 @@ LOGOUT_REDIRECT_URL = 'dashboard:login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
